11cf933b72e5d3cf7fdab1da32590e6df4a9f069
David Onoh <daveeddrumz@gmail.com>
2022-06-14 16:20:01 +0100

    created bottom sheet for new borns

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,9 +1,12 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
 import 'package:test_app/utils/Ui_helper.dart';
+
+import '../animations/bottom_up_animations.dart';
+import '../animations/slide_left_animation.dart';
 
 class HomeScreen extends StatefulWidget {
   static String routeName = "/home";
@@ -19,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Color(0xFFFFFBF8),
       floatingActionButton: FloatingActionButton(
-          onPressed: () =>showActionButtomSheet(context),
+          onPressed: () => showActionButtomSheet(context),
           child: Icon(
             Icons.add,
             color: Colors.white,
@@ -36,16 +39,17 @@ class _HomeScreenState extends State<HomeScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Lottie.asset("images/baby.json"),
-                  Text( 
+                  Text(
                     "New borns should be shown here.\nAdd new born by clicking on\n Add the button",
-                    style: TextStyle(color: Colors.grey,fontSize: 16.sp,fontWeight: FontWeight.w500),
-                     textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
                   )
                 ],
               ),
             ),
-
-
           ],
         ),
       ),
@@ -54,11 +58,71 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 void showActionButtomSheet(context) {
- UiHelper.customBottomSheet(
+  UiHelper.customBottomSheet(
     context: context,
     height: 348.h,
     child: Container(
-       height: 400.h,
+      height: 400.h,
+      child: SingleChildScrollView(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              "Create new born",
+              style: TextStyle(
+                  color: Color(0xFFF9923B),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16.sp),
+            ),
+          ),
+          SizedBox(height: 32.h),
+          BottomUpAnimaitons(
+            delay: 0.2,
+            child: TextFormField(
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 15.r, vertical: 9.r),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none),
+                  filled: true,
+                  hintStyle: TextStyle(color: Color(0xff88879C)),
+                  hintText: "Child's name",
+                  fillColor: Color(0XFFE6E6E6)),
+            ),
+          ),
+          SizedBox(height: 20.h),
+          BottomUpAnimaitons(
+            delay: 0.2,
+            child: TextFormField(
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 15.r, vertical: 9.r),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none),
+                  filled: true,
+                  hintStyle: TextStyle(color: Color(0xff88879C)),
+                  hintText: "Gender",
+                  fillColor: Color(0XFFE6E6E6)),
+            ),
+          ),
+           SizedBox(height: 30.h),
+           SlideInLeftAnimation(
+            delay: 0.2,
+            child: UiHelper.button(
+                Text(
+                  "Create",
+                  style: TextStyle(color: Colors.white),
+                ),
+                () {},
+               // loading: controller.isLoading,
+                color: Color(0xFFF9923B)),
+          )
+        ],
+      )),
     ),
   );
 }
